2f4da5835211e3a49eb7c816d7b7aa0d6f25627f
H8855 <madukthedoppelganger@gmail.com>
2016-12-06 20:59:24 +0200

    Add project files.

---
+++ Basket/Basket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketNS
+{
+    public class Basket
+    {
+        #region CONSTRUCTORS
+        public Basket(string name)
+        {
+            this.customer = name;
+            this.contents = new StringCollection();
+        }
+        #endregion
+        #region PROPERTIES
+        private string customer;        
+
+        public string Customer
+        {
+            get { return customer; }
+        }
+
+        private float price;
+
+        public float Price
+        {
+            get { return price; }
+        }
+
+        private StringCollection contents;
+
+        public StringCollection Contents
+        {
+            get { return contents; }
+        }
+        #endregion
+        #region METHODS
+        public void AddProduct(string product, float productPrice)
+        {
+            contents.Add(product);
+            price += productPrice;
+        }
+
+        public void DeleteProduct(string product, float productPrice)
+        {
+            contents.Remove(product);
+            price -= productPrice;
+
+        }
+
+        public float CountDiscountPrice(float percent)
+        {
+            float discount = (percent / 100);
+            return price - (price * discount);
+        }
+        #endregion
+    }
+}
---
+++ BasketTests/BasketTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasketNS;
+using System.Collections.Specialized;
+
+namespace BasketTests
+{
+    [TestClass]
+    public class BasketTests
+    {
+        Basket keijonOstoskori;
+
+        [TestInitialize]
+        public void testInit()
+        {
+            keijonOstoskori = new Basket("Keijo");
+            keijonOstoskori.AddProduct("Kissa", 10);
+            keijonOstoskori.AddProduct("Pasi", 10);            
+        }
+
+        [TestCleanup]
+        public void testClean()
+        {
+            keijonOstoskori = null;
+        }
+
+        [TestMethod]
+        public void TestCustomerIsString()
+        {
+            Assert.IsInstanceOfType(keijonOstoskori.Customer, typeof(string), "Variable customer is not a string.");
+        }
+
+        [TestMethod]
+        public void TestContentsIsList()
+        {
+            Assert.IsInstanceOfType(keijonOstoskori.Contents, typeof(StringCollection), "Variable contents is not a StringCollection.");
+        }
+
+        [TestMethod]
+        public void TestPriceIsNumber()
+        {
+            Assert.IsInstanceOfType(keijonOstoskori.Price, typeof(float), "Variable price is not a float.");
+        }
+
+        [TestMethod]
+        public void TestCanAddProduct()
+        {
+            keijonOstoskori.AddProduct("kala", 5);
+            CollectionAssert.Contains(keijonOstoskori.Contents,"kala", "AddProduct did not add the product to the list.");
+        }
+
+        [TestMethod]
+        public void TestCanRemoveProduct()
+        {
+            keijonOstoskori.DeleteProduct("Pasi", 10);
+            CollectionAssert.DoesNotContain(keijonOstoskori.Contents, "Pasi", "DeleteProduct did not delete the product from the list.");
+        }
+    }
+}
